24bbf2d22487de098b8acadb2581d4e4e14628c8
reidizhupa <reidizhupa56@gmail.com>
2026-01-27 09:24:59 +0900

    v1

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import "./globals.css";
+import { Roboto, Playfair_Display } from "next/font/google";
+
+const roboto = Roboto({
+	subsets: ["latin"],
+	weight: ["300", "400", "500", "700"],
+	variable: "--font-roboto",
+});
+
+const playfair = Playfair_Display({
+	subsets: ["latin"],
+	variable: "--font-playfair",
+});
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang="en" className="light">
+			<head>
+				<link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+			</head>
+			<body className={`${roboto.variable} ${playfair.variable} bg-background-light dark:bg-background-dark`}>{children}</body>
+		</html>
+	);
+}

b786ac100eb140feaa8e10b6402cea30ee521e0d
reidizhupa <reidizhupa56@gmail.com>
2026-01-30 22:09:18 +0900

    fix

--- src/components/homepage/heroSection.tsx
+++ src/components/homepage/heroSection.tsx
@@ -8,17 +8,17 @@ export default function Hero() {
 
 	return (
 		<section className="max-w-[1200px] mx-auto px-6 py-16 md:py-20">
-			<div className="grid grid-cols-1 md:grid-cols-2 gap-16 items-center">
+			<div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-16 items-center">
 				{/* Text content */}
 				<div className="flex flex-col gap-8 order-2 md:order-1">
-					<h1 className="text-6xl md:text-6xl font-black text-[#111318]">
+					<h1 className="text-4xl md:text-6xl font-black text-[#111318]">
 						{t("hero.title.line1")} <br />
 						{t("hero.title.line2")}
 						<br />
 						<span className="text-[#1754cf] italic">{t("hero.title.highlight")}</span>
 					</h1>
 
-					<p className="text-lg text-gray-600 max-w-[500px] leading-relaxed">{t("hero.description")}</p>
+					<p className="text-md md:text-lg text-gray-600 max-w-[500px] leading-relaxed">{t("hero.description")}</p>
 
 					<div className="flex flex-col sm:flex-row gap-4">
 						<Link href="#book-consultation" className="bg-[#1754cf] text-white text-base font-bold px-10 py-3 rounded-xl shadow-xl shadow-[#1754cf]/20 hover:scale-[1.02] transition-transform text-center">
